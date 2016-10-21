8301c3377f563d052c619f0b2faa3d58e2245e06
Tom Rice <t.rice90@gmail.com>
2016-10-20 22:39:42 -0400

    simple skeleton correction_factor.py

---
+++ correction_factor/correction_factor.py
@@ -0,0 +1,13 @@
+""" This is the core code package for correction_factor. """
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+# i'm attempting python 3 compatibility.
+
+def correction_factor(some_information_about_molecule_and_lines, kinetic_temperature, number_density):
+    """ Function docstring """
+
+    pass
+
+
