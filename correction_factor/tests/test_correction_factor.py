fc551b78352f4491b618d538b83c0ea1b629734b
Tom Rice <t.rice90@gmail.com>
2016-10-20 22:40:01 -0400

    simple dumb unfinished test

---
+++ correction_factor/tests/test_correction_factor.py
@@ -0,0 +1,13 @@
+import pytest
+
+from ..correction_factor import correction_factor
+
+def test_correction_factor():
+
+	expected = {}
+
+	actual = correction_factor(dummy_arguments)
+
+	assert expected == actual
+
+	
