3a1775ba2121fefae66135dbce9cf0624319f78d
Tom Rice <t.rice90@gmail.com>
2016-12-13 18:52:17 -0500

    I have added a kwarg to control how many population levels are included in the calculation.
    This is largely in order to be able to reproduce correction factor plots from previous works which use fewer levels than what RADEX computes (e.g. Plume '12 only uses the lowest 18 levels even though my version of RADEX gives 30 transitions)

--- correction_factor/correction_factor_2.py
+++ correction_factor/correction_factor_2.py
@@ -11,13 +11,15 @@ from __future__ import (absolute_import, division, print_function,
 import numpy as np
 
 
-def correction_factor_given_radex_table(input_table, indices_of_observed_states):
+def correction_factor_given_radex_table(input_table, indices_of_observed_states, included_levels=np.inf):
     """ Takes a RADEX output table and does the correction-factor magic on it. """
 
     Nupper_array = input_table['upperlevelpop']
     Nlower_array = input_table['lowerlevelpop']
 
-    total_population = np.sum(Nlower_array)
+    top_level = min(included_levels, len(input_table))
+
+    total_population = np.sum(Nlower_array[:top_level])
     observed_population = np.sum(Nupper_array[indices_of_observed_states])
 
     fraction_observed = observed_population/total_population
