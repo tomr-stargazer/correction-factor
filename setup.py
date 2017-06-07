9532fabcb17b67eecbb463d88c910ce63b11000b
Tom Rice <t.rice90@gmail.com>
2017-06-07 12:48:14 -0400

    now we have a simple setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='correction_factor',
+      version='0.0.1',
+      description='helps compute correction factors from radex outputs',
+      url='https://github.com/tomr-stargazer/correction-factor',
+      author='Tom Rice',
+      author_email='t.rice90@gmail.com',
+      packages=['correction_factor'],
+      zip_safe=False)
